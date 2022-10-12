0d34af68017fb5ba48f63d23e4ab79c7206c3219
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-10-12 13:06:53 +0600

    update statistics

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -3,30 +3,32 @@ import { TopicsContext } from './Root';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const Statistics = () => {
     const quizTopics = useContext(TopicsContext);
-    const datas = quizTopics.data;
+    const {data} = quizTopics;
     return (
-        <div className='mt-5 d-flex flex-column justify-content-center align-items-center'>
-                    <LineChart width={400} height={350}
-                        data={datas.map(data => data)}
-                        margin={{
-                            top: 5,
-                            right: 20,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        {/* <Line type="monotone" dataKey="total" stroke="#82ca9d" /> */}
-                    </LineChart>
-                    <br />
-                    <h2 className='text-warning'>Statistics Of Quiz</h2>
+        
+           <div style={{width:"80%",margin:"50px auto"}}>
+            <ResponsiveContainer width="100%" aspect={3}>
+                <LineChart width={400} height={350}
+                    data={data.map(data => data)}
+                    margin={{
+                        top: 5,
+                        right: 20,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    {/* <Line type="monotone" dataKey="total" stroke="#82ca9d" /> */}
+                </LineChart>
 
-         </div>
+            </ResponsiveContainer>
+           </div>
+       
     );
 };
 
